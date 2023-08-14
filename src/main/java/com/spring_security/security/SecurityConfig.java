2b0498d11ba8e2362c381387a0101a541fd8c354
kibriatuhin <tuinn78@gmail.com>
2023-08-14 14:53:40 +0600

    push in github

---
+++ src/main/java/com/spring_security/security/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.spring_security.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfig {
+    
+    //UserDetailsService
+    @Bean
+    public UserDetailsService userDetailsService(){
+        var inMemoryUserDetailsManager = new InMemoryUserDetailsManager();
+        var user = User.withUsername("Tuhin").password("123").authorities("read").build();
+        inMemoryUserDetailsManager.createUser(user);
+
+        return inMemoryUserDetailsManager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
