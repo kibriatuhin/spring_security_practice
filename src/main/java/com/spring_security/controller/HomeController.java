f80642455db74875ee8bdf4fad9ffe0d7c3886ba
kibriatuhin <tuinn78@gmail.com>
2023-08-16 17:08:47 +0600

    implementing UserDetailsService

--- src/main/java/com/spring_security/controller/HomeController.java
+++ src/main/java/com/spring_security/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-    @GetMapping("/hello")
+    @GetMapping("/hi")
     public String hiSay(){
         return "Hello security";
     }
--- src/main/java/com/spring_security/service/LoginStudent.java
+++ src/main/java/com/spring_security/service/LoginStudent.java
@@ -32,7 +32,7 @@ public class LoginStudent implements UserDetails {
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
